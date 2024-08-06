77d1f76301104420e6d7d0f215fbf8657cd0db9d
ErikWe <erikwestberg7@gmail.com>
2024-08-06 14:03:53 +0200

    Derive IParameter marker-interface

--- src/Models/IMethodParameter.cs
+++ src/Models/IMethodParameter.cs
@@ -2,8 +2,11 @@
 
 using Microsoft.CodeAnalysis;
 
+using Paraminter.Associators.Models;
+
 /// <summary>Represents a method parameter.</summary>
 public interface IMethodParameter
+    : IParameter
 {
     /// <summary>The symbol associated with the method parameter.</summary>
     public abstract IParameterSymbol Symbol { get; }
